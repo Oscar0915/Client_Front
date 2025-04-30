28fa5faa5445b73b2f61fa826759820dde2d3f41
Oscar <oscarballestach@gmail.com>
2025-04-29 19:40:57 -0500

    feat: implementation paginators

--- src/app/Components/client-data-table/client-data-table.component.ts
+++ src/app/Components/client-data-table/client-data-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,12 +8,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpErrorResponse } from '@angular/common/http';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { StatusDataClientService } from '../../core/Services/StatusClientData/status-data-client.service';
+import {  MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 
 @Component({
   selector: 'app-client-data-table',
   standalone: true,
-  imports: [MatTableModule, MatIconModule, MatButtonModule, MatProgressSpinnerModule],
+  imports: [MatTableModule, MatIconModule, MatButtonModule, MatProgressSpinnerModule, MatPaginatorModule],
   templateUrl: './client-data-table.component.html',
   styleUrl: './client-data-table.component.scss'
 })
@@ -23,11 +24,17 @@ export class ClientDataTableComponent implements OnInit {
   dataSource = new MatTableDataSource<ClientDataModel>([]);
   isLoading: boolean;
 
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  fullClientData: ClientDataModel[];
+
+
+
   constructor(private _ClientsApiServiceService: ClientsApiService, private snackBar: MatSnackBar,
     private _StatusDataClientService: StatusDataClientService) {
 
     this.displayedColumns = ['sharedKey', 'businessId', 'email', 'phone', 'date', 'actions'];
     this.isLoading = true;
+    this.fullClientData = [];
   }
 
   ngOnInit(): void {
@@ -35,8 +42,10 @@ export class ClientDataTableComponent implements OnInit {
 
     this._StatusDataClientService.clients$.subscribe(data => {    
       this.dataSource.data = data;
+      this.fullClientData = data;
     });
   }
+  
 
   public GetClients(): void {
     this.isLoading = false;
@@ -56,6 +65,18 @@ export class ClientDataTableComponent implements OnInit {
       }
     );
   }
+
+  onPageChange(event: any): void {
+    const pageIndex = event.pageIndex;
+    const pageSize = event.pageSize;
+    const startIndex = pageIndex * pageSize;
+    const endIndex = startIndex + pageSize;
+  
+    const pagedData = this.fullClientData.slice(startIndex, endIndex); // usa fullClientData
+    this.dataSource = new MatTableDataSource<ClientDataModel>(pagedData);
+    this.dataSource.paginator = this.paginator;
+  }
+  
 
 
   private openSnackBar(message: string, action: string) {
